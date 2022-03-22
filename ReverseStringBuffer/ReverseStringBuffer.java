56440b2d893683ba6e739da673089c76faded512
Shalini Tirkey <36034406+ShaluRose@users.noreply.github.com>
2022-03-22 11:29:54 +0530

    Update ReverseStringBuffer.java

--- ReverseStringBuffer/ReverseStringBuffer.java
+++ ReverseStringBuffer/ReverseStringBuffer.java
@@ -4,31 +4,11 @@ import java.util.Scanner;
 
 public class ReverseStringBuffer {
     public static void main(String[] args){
-        System.out.println("Enter your first String: ");
-        Scanner str1 = new Scanner(System.in);
-        String word = str1.nextLine();
-        System.out.println("Enter your second String: ");
-        Scanner str2 = new Scanner(System.in);
-        String anagram = str2.nextLine();
-
-        System.out.println("Anagram? : " + isAnagramUsingStringMethod(word, anagram));
-
-    }
-    public static boolean isAnagramUsingStringMethod(String word, String anagram)
-    {
-        if(word.length() != anagram.length())
-        return false;
-
-        for(int i =0; i<word.length(); i++){
-            char c = word.charAt(i);
-            int index = anagram.indexOf(c);
-
-            if(index != -1){
-                anagram = anagram.substring(0, index) + anagram.substring(index +1);
-            }else
-                return false;
-        }
-        return anagram.isEmpty();
+       System.out.println("Enter a String");
+        Scanner str = new Scanner(System.in);
+        String string = str.nextLine();
+        StringBuffer strB = new StringBuffer(string);
+        System.out.println("The reverse of your entered String: "+strB.reverse());
     }
 }
 
